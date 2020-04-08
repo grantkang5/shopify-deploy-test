94f4ed825382c8764d1e13713f1793f00fe1a993
grantkang5 <g1k05j@gmail.com>
2020-04-08 13:45:05 -0700

    Add fonts

--- slate.config.js
+++ slate.config.js
@@ -5,14 +5,28 @@
 
 const path = require('path');
 
+const alias = {
+  'fonts': path.resolve('./src/assets/fonts')
+}
+
 module.exports = {
   'cssVarLoader.liquidPath': ['src/snippets/css-variables.liquid'],
+  'slateTools': {
+    extends: {
+      dev: {
+        resolve: { alias }
+      },
+      prod: { 
+        resolve: { alias }
+      }
+    }
+  },
   'webpack.extend': {
     resolve: {
       alias: {
         jquery: path.resolve('./node_modules/jquery'),
         'lodash-es': path.resolve('./node_modules/lodash-es'),
       },
-    },
+    }
   },
 };
